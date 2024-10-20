a50821daf6a1f8b242755ddc4c7e16101a8e84ab
Cesar Vallejos <cdvallejos02@gmail.com>
2024-10-20 18:18:34 -0300

    second commit

---
+++ src/components/CardWidget/CardWidget.jsx
@@ -0,0 +1,14 @@
+// import logo from "../../assets/vite.svg"
+import logo from "../../assets/icons8-cart-32.png"
+
+function CardWidget() {
+  return (
+    <a className="nav-link" href="#">
+     
+       <img src={logo} alt="" />
+       <span>0</span>
+    </a>
+  )
+}
+
+export default CardWidget
---
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,11 @@
+import './ItemListContainer.css'
+
+function ItemListContainer({ text }) {
+  return (
+    <div>
+      <p className="textGreet">{text}</p>
+    </div>
+  )
+}
+
+export default ItemListContainer
